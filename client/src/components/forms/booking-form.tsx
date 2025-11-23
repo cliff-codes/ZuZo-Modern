b813a4e2b6ed59cb80e24c965a854e1d1f5ff330
gdebrah <19419250-gdebrah@users.noreply.replit.com>
2025-11-23 20:39:02 +0000

    Restored to '8c52a98bef085454f7e854bfc24c57d46ed2a188'
    Replit-Restored-To: 8c52a98bef085454f7e854bfc24c57d46ed2a188

--- client/src/components/forms/booking-form.tsx
+++ client/src/components/forms/booking-form.tsx
@@ -33,7 +33,7 @@ export function BookingForm() {
           className="relative overflow-hidden"
           style={{
             height: "500px",
-            marginTop: "-120px",
+            marginTop: "-80px",
           }}
         >
           <iframe
@@ -41,10 +41,10 @@ export function BookingForm() {
             data-testid="cal-booking-embed"
             style={{
               width: "100%",
-              height: "620px",
+              height: "580px",
               border: "none",
               position: "relative",
-              top: "120px",
+              top: "80px",
             }}
             title="Book a Demo with ZuZo"
           />
